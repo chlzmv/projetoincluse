57f1f96dbf0ceaa979bc840e6b1a0e935799936d
chlzmv <charlize.ifes.22@gmail.com>
2023-06-13 15:36:45 -0300

    att

--- dbconexao.php
+++ dbconexao.php
@@ -11,7 +11,10 @@ $connect = mysqli_connect($servername,$username,$password,$db_name);
 //codifica com o caracteres ao manipular dados do banco de dados
 //mysqli_set_charset($connect, "utf8");
 
-if(mysqli_connect_error()):
+if(mysqli_connect_error()){
 	echo "Falha na conexão: ". mysqli_connect_error();
-endif;
-?>
+}else;
+
+
+//eu amo muito hanaa tipo muito mesmooooooo
+?> 
