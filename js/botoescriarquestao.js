c705fa931ecebb5068271b0aef6dc44b66283b63
chlzmv <charlize.ifes.22@gmail.com>
2023-06-16 17:18:16 -0300

    att

--- js/botoescriarquestao.js
+++ js/botoescriarquestao.js
@@ -1,60 +1,34 @@
 
-// var controleCampo = 1;
-
-// function adicionarCampo() {
-//   controleCampo++;
-//   console.log(controleCampo);
-//   var tabela = document.getElementById('table');
-//   tabela.insertAdjacentHTML(
-//     'beforeend',
-//     '<table id="campo' + controleCampo +
-//       '"><tr><td><input type="checkbox" id="checkbox"></td><td><textarea id="checkText" placeholder="Insira o texto"></textarea></td><td><span id="check" class="material-symbols-outlined">check</span></td><td><span id="add2" class="material-symbols-outlined" onclick="adicionar()">add</span></td><td><span id="' +
-//       controleCampo +
-//       '" class="material-symbols-outlined" onclick="removerCampo(' +
-//       controleCampo +
-//       ')">delete</span></td></tr></table>'
-//   );
-// }
-
-// function removerCampo(idCampo){
-//     //console.log("Campo remover: " + idCampo);
-//     document.getElementById('campo' + idCampo).remove();
-// }
-
-{/* <div id="table">
-            
-                
-                                                                                                                                <input type="checkbox" id="checkbox1" placeholder="check1">
-                                                                                                                                
-                                                                                                                            
-                                                                                                                                <textarea  id="checkText" cols="30" rows="1" placeholder="Insira o texto"></textarea>
-                                                                                                                                
-                                                                                                                                
-                                                                                                                                <span  class="material-symbols-outlined" id="check" >check</span>
-                                                                                                                                
-                                                                                                                                
-                                                                                                                                <span class="material-symbols-outlined" id="add2"  onclick="adicionarCampo()">add</span>
-                                                                                                                                
-                                                                                                                                
-                                                                                                                                <span id="delete" class="material-symbols-outlined" onclick="removerCampo()">delete</span>
-            
-            
-        
-    </div> */}
+//adicionar e apagar iten de resposta
 
 var controleCampo = 1;
 function adicionarCampo() {
-    controleCampo++;
-    //console.log(controleCampo);
-
+    controleCampo++;   
     document.getElementById('table').insertAdjacentHTML('beforeend', '<div class="table" id="campo' + controleCampo + '"> <input type="checkbox" id="checkbox1" placeholder="check1"> <textarea  id="checkText" cols="30" rows="1" placeholder="Insira o texto"></textarea> <span  class="material-symbols-outlined" id="check" >check</span> <span class="material-symbols-outlined" id="add2"  onclick="adicionarCampo()">add</span> <span class="material-symbols-outlined" id="' + controleCampo + '" onclick="removerCampo(' + controleCampo + ')"> delete </span></div>');
 }
 
-function removerCampo(idCampo){
-    //console.log("Campo remover: " + idCampo);
+function removerCampo(idCampo){   
     document.getElementById('campo' + idCampo).remove();
 }
 
+//adicionar e apagar questao completa
+
+var controleQuestao = 1;
+function adicionarQuestao() {
+    controleQuestao++;
+    console.log(controleQuestao);
+    document.getElementById('section').insertAdjacentHTML('afterbegin', '<div class="section" id="questao' + controleQuestao + '"> <input type="text" id="numQuest" placeholder="N° Questão" ><div class="inputSV"><select id="formSelect"><option selected disabled>Tipo da Questão:</option><option value="1">Resposta Única</option><option value="2">Múltipla Escolha</option></select><input type="text" id="valorQuest" placeholder="Valor"></div></section><section class="divCaixaTexto"><textarea id="caixaTexto" cols="30" rows="20" placeholder="Insira o texto"></textarea></section><div id="table"><input type="checkbox" id="checkbox1" placeholder="check1"><textarea  id="checkText" cols="30" rows="1" placeholder="Insira o texto"></textarea><span id="check" class="material-symbols-outlined">check</span><span id="add2" class="material-symbols-outlined" onclick="adicionarCampo()">add</span><span class="material-symbols-outlined" id="' + controleCampo + '" onclick="removerCampo(' + controleCampo + ')"> delete </span>  </div> </section>');
+}
+
+
+if(controleQuestao<2){
+    funcçao dditemderesposta
+    controcamo ++
+
+}
+elif(controleQuestao>1){
+    
+}
 
 // <!DOCTYPE html>
 // <html>
